25a3223da2e90b589af3b1617bdc9dcf60abf532
Victor H. García <victorhugogarcia13d@gmail.com>
2025-06-20 18:48:00 -0600

    GUNDI-4223: Update REQUESTED_PROPERTIES

--- app/actions/client.py
+++ app/actions/client.py
@@ -7,8 +7,8 @@ from datetime import datetime, timezone, timedelta
 
 logger = logging.getLogger(__name__)
 
-REQUESTED_PROPERTIES = 'u.unit_id,u.unit_name,a.model,o.name,ac.gps_time,ac.status,ac.latitude,ac.longitude,ac.current_odometer_reading,' \
-                           'ac.speed,ac.hdop,ac.altitude,ac.heading,u.description'
+REQUESTED_PROPERTIES = 'unit_id,unit_name,a.model,organization_name,real_time_GPS_Time,real_time_status,real_time_Latitude,real_time_Longitude,real_time_Distance,real_time_Speed,ac.hdop,ac.altitude,ac.heading,u.description'
+
 
 
 class GalooliGeneralErrorException(Exception):
